fe21d3da71270ac5b78b9223827e3aac6aaf2413
comproterreno <comproterreno4@gmail.com>
2025-01-22 17:38:48 -0800

    Mailing

--- app/store/Store.tsx
+++ app/store/Store.tsx
@@ -10,12 +10,23 @@ export default function Store() {
   const searchParams = useSearchParams();
 
   const products = [
-    { id: 1, name: "Kenko Air", price: "$10.00", image: "/images/product1.jpg", category: "air" },
-    { id: 2, name: "Repuesto Nikken", price: "$20.00", image: "/images/product2.jpg", category: "repuestos" },
-    { id: 3, name: "Filtro de Agua", price: "$30.00", image: "/images/product3.jpg", category: "water" },
+    { id: 0, name: "PiMag Hydrogen", price: "$30.00", image: "/images/product3.jpg", category: "agua" },
+    { id: 1, name: "Pi Water", price: "$6451.00", image: "/images/piWater.jpeg", category: "agua" },
+    { id: 2, name: "Optimizer", price: "$20.00", image: "/images/product2.jpg", category: "agua" },
+    { id: 3, name: "Waterfall", price: "$30.00", image: "/images/product3.jpg", category: "agua" },
+    { id: 4, name: "Kenko Air Purifier", price: "$30.00", image: "/images/product3.jpg", category: "aire" },
+    { id: 5, name: "Kenko Sleep", price: "$30.00", image: "/images/product3.jpg", category: "descanso" },
+    { id: 6, name: "Kenko Sleep Comforter", price: "$30.00", image: "/images/product3.jpg", category: "descanso" },
+    { id: 7, name: "Repuesto Optimizer", price: "$30.00", image: "/images/product3.jpg", category: "repuestos" },
+    { id: 8, name: "Repuesto Kenko Air", price: "$30.00", image: "/images/product3.jpg", category: "repuestos" },
+    { id: 9, name: "Repuesto PiMag Aqua Pour", price: "$30.00", image: "/images/product3.jpg", category: "repuestos"},
+    { id: 10, name: "Repuesto PiMag Waterfall", price: "$30.00", image: "/images/product3.jpg", category: "repuestos"},
+    { id: 11, name: "Repuesto PiMag Piwater", price: "$30.00", image: "/images/product3.jpg", category: "repuestos"},
+    { id: 12, name: "Repuesto PiMag", price: "$30.00", image: "/images/product3.jpg", category: "repuestos"},
+
   ];
 
-  const categories = ["all", "air", "water", "repuestos"];
+  const categories = ["todas", "aire", "descanso", "agua", "repuestos"];
 
   useEffect(() => {
     const category = searchParams.get("category") || "all";
