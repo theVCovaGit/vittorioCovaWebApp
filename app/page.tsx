681523685e70f3cabbbf4f6e7d2407606b628341
theJRFGitHub <jrf2421@gmail.com>
2025-04-10 22:52:18 -0600

    Initial push for Vittorio Cova WebApp

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="flex-1 w-full bg-[#5c4b4a] text-black font-basica flex items-start justify-start px-10 md:px-24 lg:px-[409px] pt-[160px] sm:pt-[180px] md:pt-[200px]">
-    <div className="flex flex-col space-y-6 absolute left-[26.25rem] z-[60]">
+    <div className="flex flex-col space-y-6 absolute top-[10.375rem] left-[26.25rem] z-[60]">
         <a
           href="/architecture"
           className="text-[1.9rem] md:text-[2rem] tracking-[-0.02em] font-normal w-fit text-[#fef4dc] no-underline hover:opacity-80 transition-opacity duration-200 z-[60]"
