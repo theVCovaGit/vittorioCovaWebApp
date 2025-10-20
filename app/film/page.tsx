f595e02b16eb5df2070d099cd1d723b7718ad880
theJRFGitHub <jrf2421@gmail.com>
2025-10-20 13:28:51 -0600

    main page

--- app/film/page.tsx
+++ app/film/page.tsx
@@ -4,7 +4,15 @@ export default function Film() {
   return (
     <div className="min-h-screen bg-[#5c4b4a] relative overflow-hidden">
       {/* Film Strip Container */}
-      <div className="film-strip-container absolute top-1/2 left-0 transform -translate-y-1/2 w-[400vw] h-[500px] overflow-x-auto overflow-y-hidden">
+      <div 
+        className="film-strip-container absolute top-1/2 left-0 transform -translate-y-1/2 w-screen h-[500px] overflow-x-scroll overflow-y-hidden scrollbar-hide"
+        style={{ 
+          scrollBehavior: 'smooth',
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+          WebkitOverflowScrolling: 'touch'
+        }}
+      >
         {/* Film Strip SVG */}
         <div className="relative w-full h-full flex">
           <img 
