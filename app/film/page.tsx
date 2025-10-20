4051aef0395d742f2859a96225b0d5978aaec74b
theJRFGitHub <jrf2421@gmail.com>
2025-10-20 13:33:05 -0600

    main page

--- app/film/page.tsx
+++ app/film/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Film() {
   return (
-    <div className="min-h-screen bg-[#5c4b4a] relative overflow-hidden">
+    <div className="min-h-screen bg-[#2d2f38] relative overflow-hidden">
       {/* Film Strip Container */}
       <div 
         className="film-strip-container absolute top-1/2 left-0 transform -translate-y-1/2 w-screen h-[500px] overflow-x-scroll overflow-y-hidden scrollbar-hide"
