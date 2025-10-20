9cfa215b291782f5369afd8c68b08f4482890570
theJRFGitHub <jrf2421@gmail.com>
2025-10-20 13:21:19 -0600

    main page

--- app/film/page.tsx
+++ app/film/page.tsx
@@ -4,14 +4,18 @@ export default function Film() {
   return (
     <div className="min-h-screen bg-[#5c4b4a] relative overflow-hidden">
       {/* Film Strip Container */}
-      <div className="film-strip-container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[200vw] h-[500px] overflow-x-auto overflow-y-hidden">
+      <div className="film-strip-container absolute top-1/2 left-0 transform -translate-y-1/2 w-[400vw] h-[500px] overflow-x-auto overflow-y-hidden">
         {/* Film Strip SVG */}
-        <div className="relative w-full h-full flex items-center justify-center">
+        <div className="relative w-full h-full flex">
           <img 
             src="/assets/film.svg" 
             alt="Film Strip" 
-            className="h-full w-auto object-contain"
-            style={{ minWidth: '100%' }}
+            className="h-full w-auto object-contain flex-shrink-0"
+          />
+          <img 
+            src="/assets/film.svg" 
+            alt="Film Strip" 
+            className="h-full w-auto object-contain flex-shrink-0"
           />
         </div>
       </div>
