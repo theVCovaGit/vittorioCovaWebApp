0d1e1614bd913aa7fd100058c106797065685af0
theJRFGitHub <jrf2421@gmail.com>
2025-10-20 15:01:44 -0600

    main page

--- app/architecture/page.tsx
+++ app/architecture/page.tsx
@@ -2,8 +2,31 @@
 
 export default function Architecture() {
   return (
-    <div className="min-h-screen bg-[#2d2f38]">
-      {/* Architecture strip will go here */}
+    <div className="min-h-screen bg-[#fff5e0] relative overflow-hidden">
+      {/* Film Strip Container */}
+      <div 
+        className="film-strip-container absolute top-1/2 left-0 transform -translate-y-1/2 w-screen h-[500px] overflow-x-scroll overflow-y-hidden scrollbar-hide"
+        style={{ 
+          scrollBehavior: 'smooth',
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+          WebkitOverflowScrolling: 'touch'
+        }}
+      >
+        {/* Film Strip SVG */}
+        <div className="relative w-full h-full flex">
+          <img 
+            src="/assets/scroll.svg" 
+            alt="Film Strip" 
+            className="h-full w-auto object-contain flex-shrink-0"
+          />
+          <img 
+            src="/assets/scroll.svg" 
+            alt="Film Strip" 
+            className="h-full w-auto object-contain flex-shrink-0"
+          />
+        </div>
+      </div>
     </div>
   );
 }
