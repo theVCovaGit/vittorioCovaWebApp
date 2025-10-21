90f58bb9308d6c5dd49ed649d35857327368dd4d
theJRFGitHub <jrf2421@gmail.com>
2025-10-21 12:03:48 -0600

    main page

--- app/architecture/page.tsx
+++ app/architecture/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Architecture() {
   return (
-    <div className="min-h-screen bg-[#fff5e0] relative overflow-hidden">
+    <div className="min-h-screen bg-[#fff5e0] relative overflow-visible">
       {/* Film Strip Container */}
       <div 
         className="film-strip-container absolute top-1/2 left-0 transform -translate-y-1/2 w-screen h-[700px] overflow-x-scroll overflow-y-visible scrollbar-hide"
@@ -26,39 +26,39 @@ export default function Architecture() {
             <img 
               src="/assets/tape1.svg" 
               alt="Tape" 
-              className="absolute -top-12 left-[5%] w-48 h-16 opacity-80 transform rotate-[2deg] z-50"
+              className="absolute -top-4 left-[5%] w-48 h-16 opacity-80 transform rotate-[2deg] z-[9999]"
             />
             <img 
               src="/assets/tape2.svg" 
               alt="Tape" 
-              className="absolute -top-12 left-[25%] w-48 h-16 opacity-80 transform rotate-[-5deg] z-50"
+              className="absolute -top-4 left-[25%] w-48 h-16 opacity-80 transform rotate-[-5deg] z-[9999]"
             />
             <img 
               src="/assets/tape3.svg" 
               alt="Tape" 
-              className="absolute -top-12 left-[45%] w-48 h-16 opacity-80 transform rotate-[4deg] z-50"
+              className="absolute -top-4 left-[45%] w-48 h-16 opacity-80 transform rotate-[4deg] z-[9999]"
             />
             <img 
               src="/assets/tape4.svg" 
               alt="Tape" 
-              className="absolute -top-12 left-[65%] w-48 h-16 opacity-80 transform rotate-[-2deg] z-50"
+              className="absolute -top-4 left-[65%] w-48 h-16 opacity-80 transform rotate-[-2deg] z-[9999]"
             />
             <img 
               src="/assets/tape5.svg" 
               alt="Tape" 
-              className="absolute -top-12 left-[85%] w-48 h-16 opacity-80 transform rotate-[3deg] z-50"
+              className="absolute -top-4 left-[85%] w-48 h-16 opacity-80 transform rotate-[3deg] z-[9999]"
             />
             <img 
               src="/assets/tape6.svg" 
               alt="Tape" 
-              className="absolute -top-12 left-[15%] w-48 h-16 opacity-80 transform rotate-[-4deg] z-50"
+              className="absolute -top-4 left-[15%] w-48 h-16 opacity-80 transform rotate-[-4deg] z-[9999]"
             />
           </div>
           
           <img 
             src="/assets/scroll.svg" 
             alt="Architecture Scroll" 
-              className="h-[500px] w-auto object-contain flex-shrink-0"
+            className="h-[500px] w-auto object-contain flex-shrink-0"
           />
         </div>
       </div>
