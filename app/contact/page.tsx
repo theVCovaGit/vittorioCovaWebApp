1b48faf7cd90e4beec07f4c7d9ab95464f2fb3e4
theJRFGitHub <jrf2421@gmail.com>
2025-10-23 19:49:13 -0600

    about fingie

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -35,13 +35,49 @@ export default function Contact() {
         <InteractiveFingie />
       </div>
 
+      {/* Contact Information - Centered */}
+      <div className="absolute top-[8rem] left-1/2 transform -translate-x-1/2 text-[#fef4dc] font-electrolize text-center">
+        <div className="flex items-center justify-center gap-3 mb-4">
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="text-[#fef4dc]">
+            <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+          </svg>
+          <span className="text-lg">@vittoriocova_studio</span>
+        </div>
+        
+        <div className="flex items-center justify-center gap-3">
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="text-[#fef4dc]">
+            <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+          </svg>
+          <span className="text-lg">studio@vittoriocova.com</span>
+        </div>
+      </div>
+
       {/* Signature in the center */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[1001]">
         <img 
           src="/assets/signatureLight.svg" 
           alt="Vittorio Cova Signature" 
-          className="w-auto h-32 sm:h-40 md:h-48"
+          className="w-auto h-20 sm:h-24 md:h-28"
         />
+      </div>
+
+      {/* Locations - Lower Center */}
+      <div className="absolute top-[65%] left-1/2 transform -translate-x-1/2 text-[#fef4dc] font-electrolize text-center">
+        <div className="text-lg">
+          <span>Mexico City</span>
+          <span className="mx-2">|</span>
+          <span>Houston</span>
+          <span className="mx-2">|</span>
+          <span>Florence</span>
+        </div>
+      </div>
+
+      {/* Copyright - Bottom Center */}
+      <div className="absolute bottom-[4rem] left-1/2 transform -translate-x-1/2 text-[#fef4dc] font-electrolize text-center">
+        <div className="text-sm">
+          © Vittorio Cova Studio<br />
+          All rights reserved.
+        </div>
       </div>
 
     </main>
