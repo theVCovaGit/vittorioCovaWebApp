cd9f9d89025a9edeb0e2395975a61abe88ebb697
theJRFGitHub <jrf2421@gmail.com>
2025-10-25 12:11:36 -0600

    contact

--- components/footerMobile.tsx
+++ components/footerMobile.tsx
@@ -10,7 +10,7 @@ export default function FooterMobile() {
 
   return (
     <footer className={`fixed bottom-0 left-0 w-full font-minecraft z-50 px-4 pt-8 pb-4 ${isFooterPage ? 'bg-[#302120]' : 'bg-[#5c4b4a]'}`}>
-      <div className="flex flex-col items-center space-y-3">
+      <div className="flex flex-col items-center space-y-3 scale-[0.8]">
         {/* Barcode - Smaller for mobile */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
