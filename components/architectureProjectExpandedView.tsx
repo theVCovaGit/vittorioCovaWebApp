2bcd0c57fdf16d771c3c90553a897ec77df55c6f
theJRFGitHub <jrf2421@gmail.com>
2025-11-04 17:50:22 -0600

    arch expanded view

--- components/architectureProjectExpandedView.tsx
+++ components/architectureProjectExpandedView.tsx
@@ -130,7 +130,7 @@ export default function ArchitectureProjectExpandedView({
             <div className="text-[#fbef56] font-electrolize font-bold text-xl">
               {project.city.toUpperCase()}
             </div>
-            <div className="text-white font-microextend font-bold text-3xl mt-1 transform -translate-y-2">
+            <div className="text-[#fff5e0] font-microextend font-bold text-3xl mt-1 transform -translate-y-2">
               {project.country.toUpperCase()}
             </div>
             <div className="text-[#fbef56] font-electrolize font-bold text-lg mt-1 transform translate-y-4">
