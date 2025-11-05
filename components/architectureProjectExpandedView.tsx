64b049feeaec04df5f13027c27350508b8061f3c
theJRFGitHub <jrf2421@gmail.com>
2025-11-05 15:23:38 -0600

    arch expanded view mobile

--- components/architectureProjectExpandedView.tsx
+++ components/architectureProjectExpandedView.tsx
@@ -90,7 +90,7 @@ export default function ArchitectureProjectExpandedView({
   if (loading || !imagesPreloaded) {
     return (
       <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
-        <div className="text-white text-xl font-microextend">Loading ...</div>
+        <div className={`text-white font-microextend ${isMobile ? 'text-lg' : 'text-xl'}`}>Loading ...</div>
       </div>
     );
   }
