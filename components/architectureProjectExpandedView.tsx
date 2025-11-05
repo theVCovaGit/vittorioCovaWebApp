01f12795c6ce0bdc9aec06b151b5db3f84dad4fb
theJRFGitHub <jrf2421@gmail.com>
2025-11-05 16:03:04 -0600

    arch expanded view mobile

--- components/architectureProjectExpandedView.tsx
+++ components/architectureProjectExpandedView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useIsMobile } from "@/hooks/useMediaQuery";
 
 interface ArchitectureProject {
