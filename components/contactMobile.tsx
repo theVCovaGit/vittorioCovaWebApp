ee1aeb1bffa558ab729468831d0eefd86e28b60d
theJRFGitHub <jrf2421@gmail.com>
2025-11-18 13:45:19 -0600

    mobile fixes: contact

--- components/contactMobile.tsx
+++ components/contactMobile.tsx
@@ -34,7 +34,7 @@ export default function ContactMobile() {
       <div style={{ 
         position: 'absolute', 
         right: '-7.2rem', 
-        bottom: 'calc(var(--barcode-bottom-offset, 80px) + 18vh)'
+        bottom: 'calc(var(--barcode-bottom-offset, 80px) + 28vh)'
       }}>
         <ContactLabel
           bottom="bottom-0"
