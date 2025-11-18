15a94c7399de624053d15bd466c23ee91599ae73
theJRFGitHub <jrf2421@gmail.com>
2025-11-18 12:28:49 -0600

    mobile fixes

--- components/aboutMobile.tsx
+++ components/aboutMobile.tsx
@@ -34,7 +34,7 @@ export default function AboutMobile() {
       <div style={{ 
         position: 'absolute', 
         right: '-13rem', 
-        bottom: 'calc(var(--barcode-bottom-offset, 80px) - 10.5vh)'
+        bottom: 'calc(var(--barcode-bottom-offset, 80px) - 9vh)'
       }}>
         <AboutLabel
           bottom="bottom-0"
