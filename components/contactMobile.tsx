ebed8fd5d7ac2072b9810cd90a6c680d865adecd
theJRFGitHub <jrf2421@gmail.com>
2026-02-02 12:15:17 -0600

    about news contact

--- components/contactMobile.tsx
+++ components/contactMobile.tsx
@@ -51,7 +51,7 @@ export default function ContactMobile() {
           left: "min(calc(var(--barcode-right, 100vw) + 3.5rem), calc(100vw - 4rem))",
           bottom: "var(--barcode-bottom-offset, 80px)",
           width: "80px",
-          transform: "translateY(calc(-100% - 9rem))",
+          transform: "translateY(calc(-100% - 14rem))",
         }}
       >
         <img
